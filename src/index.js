0470de7899c22052aba04bb413f7f0c425797f15
kandelmilan <kandelr950p@gmail.com>
2026-03-22 12:36:34 +0545

    Added-Backend

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const DbConnection = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL); // remove old options
         console.log("DB Connected");
     } catch (error) {
         console.error("DB connection error:", error.message);
+        throw error;
     }
-
 };
 
 module.exports = DbConnection;
